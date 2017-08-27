6721cdbdaf5914712bd4231e137ff219b5447da6
jlee29 <jpal.lee7@gmail.com>
2017-08-27 10:21:55 -0700

    Got the state to post. Now need to do some reconfiguration by making the AddNote component a child component of NoteCollection

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
 class AddNote extends Component {
-  constructor() {
+  constructor(props) {
     super(props);
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleSubjectChange = this.handleSubjectChange.bind(this);
+    this.addNote = this.addNote.bind(this);
     this.state = {
       title:'',
       text:''
@@ -13,23 +14,37 @@ class AddNote extends Component {
 
 
   handleTitleChange(e) {
-    e.preventDefault();
     this.setState({title:e.target.value});
   }
 
   handleSubjectChange(e) {
-    e.preventDefault();
     this.setState({text:e.target.value});
   }
 
-  addNote(){
-    
+  addNote(e) {
+    e.preventDefault();
+    fetch('http://localhost:8000/notes', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: this.state.title,
+        text: this.state.text
+      })
+    })
+    .then(function(response) {
+      return response.json()
+    }).then(function(body) {
+      console.log(body);
+    });
   }
 
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form onSubmit={this.addNote}>
         <input type="text" onChange={this.handleTitleChange} placeholder="Title" ref="title"/>
         <input type="text" onChange={this.handleSubjectChange} placeholder="Text" ref="text" />
         <input type="submit" />
