591e21f31d186a3d7d6ba7545a6e3824ad326773
Josh <jpal.lee7e@gmail.com>
2017-09-04 21:43:50 -0700

    Adding some more formatting stuff

--- src/ArticleComponents/GetArticles.js
+++ src/ArticleComponents/GetArticles.js
@@ -14,16 +14,16 @@ class GetArticles extends Component {
   componentDidMount() {
     fetch('https://newsapi.org/v1/articles?source=' + this.state.sourceid + '&sortBy=latest&apiKey=c4dfbc715df444bd9e673e5ca0e0f005')
     .then(res => res.json())
-    .then(articles => this.setState({articles: articles.articles}));
+    .then(articles => {
+      this.setState({articles: articles.articles});
+      console.log('Retrieved the articles');
+    });
   }
-
-  on
 
   render() {
     return (
       <div>
         <h1>Articles</h1>
-        <Row>
           {this.state.articles.map((article, index) =>
             <a href={article.url} key={index}>
               <Col lg={3}>
@@ -34,7 +34,6 @@ class GetArticles extends Component {
               </Col>
             </a>
           )}
-        </Row>
       </div>
     )
   }
