9126a31c54a8ae6d35be2e7e4d8d20d5c3957c83
Josh <jpal.lee7e@gmail.com>
2017-09-26 20:02:46 -0700

    Changing the url to removing the sorting parameter

--- src/ArticleComponents/GetArticles.js
+++ src/ArticleComponents/GetArticles.js
@@ -7,13 +7,24 @@ class GetArticles extends Component {
     super(props);
     this.state = {
       articles: [],
+      articleError: false,
       sourceid: this.props.match.params.sourceid
     }
   }
 
   componentDidMount() {
-    fetch('https://newsapi.org/v1/articles?source=' + this.state.sourceid + '&sortBy=latest&apiKey=c4dfbc715df444bd9e673e5ca0e0f005')
-    .then(res => res.json())
+    fetch('https://newsapi.org/v1/articles?source=' + this.state.sourceid + '&apiKey=c4dfbc715df444bd9e673e5ca0e0f005')
+    .then(res => {
+      if(res.error){
+        this.setState({articleError: true})
+        console.log("error")
+      }
+      else {
+        console.log("no error")
+        console.log(res)
+        return res.json()
+      }
+    })
     .then(articles => {
       this.setState({articles: articles.articles});
       console.log('Retrieved the articles');
@@ -21,26 +32,34 @@ class GetArticles extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h1>Articles</h1>
-        <Grid fluid={true}>
-          <Row>
-            {this.state.articles.map((article, index) =>
-              <a href={article.url} key={index}>
-                <Col md={3}>
-                  <Thumbnail className="Article-thumbnail" src={article.urlToImage}>
-                    <h4>{article.title}</h4>
-                    <p>{article.description}</p>
-                  </Thumbnail>
-                </Col>
-              </a>
-            )}
-          </Row>
-        </Grid>
-        <footer>Made possible by the services of <a href="https://newsapi.org/">News API</a></footer>
-      </div>
-    )
+    if (!this.state.articleError) {
+      return (
+        <div>
+          <h1>Articles</h1>
+          <Grid fluid={true}>
+            <Row>
+              {this.state.articles.map((article, index) =>
+                <a href={article.url} key={index}>
+                  <Col md={3}>
+                    <Thumbnail className="Article-thumbnail" src={article.urlToImage}>
+                      <h4>{article.title}</h4>
+                      <p>{article.description}</p>
+                    </Thumbnail>
+                  </Col>
+                </a>
+              )}
+            </Row>
+          </Grid>
+          <footer>Made possible by the services of <a href="https://newsapi.org/">News API</a></footer>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Looks like these guys don't want to share their news with you</h2>
+        </div>
+      )
+    }
   }
 }
 
